4db6bd902e234eefd38d9a6eeb058d2c409d434d
Margreet-Haken <margreet.haken@gmail.com>
2017-10-11 16:37:52 +0200

    eerste commit

---
+++ test.js
@@ -0,0 +1,14 @@
+// @flow
+
+var myArray = [];
+
+for (var i = 0; i < myArray.length; i++) {
+    var obj = myArray[i];
+    
+}
+
+console.log('test');
+
+console.log('test:', test);
+
+var x = 10;
